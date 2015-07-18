6cc31d3c0b842a608ea5ad554f550ab38f4a2cac
vagrant <m.makie@fuga.ocn.ne.jp>
2015-07-18 08:12:01 +0000

    Update

---
+++ spec/a1546mm_miyauchi_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe A1546mmMiyauchi do
+  it 'has a version number' do
+    expect(A1546mmMiyauchi::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'a1546mm_miyauchi'
